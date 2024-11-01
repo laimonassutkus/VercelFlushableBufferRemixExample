0b7c1de039936ff36511e60ae3108c26b8e5a0cb
Laimonas Sutkus <laimonas.sutkus@gmail.com>
2024-11-01 10:55:02 +0200

    Add promise

--- app/services/logging.ts
+++ app/services/logging.ts
@@ -3,5 +3,13 @@ import asyncLog from "./asyncLog"
 
 export default function info(log: string) {
     waitUntil(asyncLog(log))
-    waitUntil(asyncLog(log))
+
+    const promise = new Promise<void>((resolve) => {
+        setTimeout(async () => {
+            try { await asyncLog(`In a promise of 10 seconds: ${log}`) }
+            finally { resolve() }
+        }, 10_000)
+    })
+
+    waitUntil(promise)
 }
